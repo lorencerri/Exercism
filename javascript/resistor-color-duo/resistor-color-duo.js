b5d5d471d610fa75cc59a6fdabe2ac4b68133b85
TrueXPixels <truexpixels@gmail.com>
2020-09-29 01:27:17 -0700

    Progress

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,16 @@
+const COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+];
+
+export const decodedValue = (colors) => {
+    return parseInt(`${COLORS.indexOf(colors[0])}${COLORS.indexOf(colors[1])}`);
+};

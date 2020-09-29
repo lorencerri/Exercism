545d8ab20644a28a5aa7d45f1f823eace34acfe1
TrueXPixels <truexpixels@gmail.com>
2020-09-28 23:39:36 -0700

    Progress

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const twoFer = () => {
-  throw new Error("Remove this statement and implement this function");
+export const twoFer = (name) => {
+    return `One for ${name || "you"}, one for me.`;
 };

546db31f4c0380f4b9438bfc6ef2027047e1a4ee
TrueXPixels <truexpixels@gmail.com>
2020-10-01 15:35:48 -0700

    Update raindrops.py

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,9 +1,5 @@
 def convert(number):
-    res = ""
-    if number % 3 == 0:
-        res += "Pling"
-    if number % 5 == 0:
-        res += "Plang"
-    if number % 7 == 0:
-        res += "Plong"
-    return res if res != "" else str(number)
+    res = "Pling" if number % 3 == 0 else ""
+    res += "Plang" if number % 5 == 0 else ""
+    res += "Plong" if number % 7 == 0 else ""
+    return str(number) if res == "" else res

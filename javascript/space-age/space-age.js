a60c7ad2bfe9a50164f482d40018374263df60e6
TrueXPixels <truexpixels@gmail.com>
2020-10-05 09:31:07 -0700

    Add space-age.js

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,14 @@
+const modifier = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+};
+
+export const age = (planet, seconds) => {
+    return Math.round((seconds / modifier[planet] / 31557600) * 100) / 100;
+};
